13a64ac0a5f32150426db86f2d019e8a2b18460f
luiz.souza <lgsouza.sp@gmail.com>
2025-01-26 16:47:08 -0300

    Definition import

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,6 +1,7 @@
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
+import { LatestInvoice } from '@/app/lib/definitions';
 import { lusitana } from '@/app/ui/fonts';
 
 export default async function LatestInvoices({
